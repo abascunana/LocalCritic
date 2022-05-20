a12feb0cef3788708f0cd1ebd5a63c5f89ae5a26
abascunana <91748309+abascunana@users.noreply.github.com>
2022-05-20 12:31:27 +0200

    Web actualizada para el login

---
+++ Web LocalCritic/localcritic.js
@@ -0,0 +1,24 @@
+
+function actualizar(){
+
+    enviar();
+}
+
+
+function enviar (){
+    var http;
+    http = new XMLHttpRequest;
+
+    http.onreadystatechange = function (){
+        if (http.readyState==4 && http.status==200){
+          
+            
+            
+
+        }
+    }
+    http.open("POST","http://localhost:8080/Localcritic/login",true);
+    http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    http.send("username="+document.getElementById("username").value+"&&password="+document.getElementById("password").value);
+ 
+}
