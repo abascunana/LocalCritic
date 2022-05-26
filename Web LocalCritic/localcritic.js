0392daf28f452294fed454dc80df690acf9a4f7c
abascunana <91748309+abascunana@users.noreply.github.com>
2022-05-26 12:40:35 +0200

    Update localcritic.js

--- Web LocalCritic/localcritic.js
+++ Web LocalCritic/localcritic.js
@@ -5,22 +5,35 @@ function enviar (){
     http.onreadystatechange = function (){
         if (http.readyState==4 && http.status==200){
             if(http.responseText == 1){
-                alert("usuario correcto");
-                usernamerg = document.getElementById("username").value;
-                localStorage.setItem("username", usernamerg);
-                alert("bienvenido, "+usernamerg);
+                alert("usuario correcto")
+                
+                localStorage.setItem('usernamerg', document.getElementById("username").value);
+                alert("bienvenido, "+ localStorage.getItem('usernamerg'))
+               
                 window.location.href = "index.html";
+              
             }
+            
            else{
-            alert("usuario incorrecto");
+            alert("usuario incorrecto")
            }
+         
         }
+        
+        
     }
+    
     http.open("POST","http://localhost:8080/Localcritic/login",true);
     http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     http.send("username="+document.getElementById("username").value+"&&password="+document.getElementById("password").value);
-   
-    return usernamerg;
   
- 
+   
+}
+
+
+function mostrarus (){
+    var username = localStorage.getItem('usernamerg');
+    alert(username);
+   
+
 }
