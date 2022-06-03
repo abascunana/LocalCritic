be338d6eaad744663976c59c91c93015512e0d1d
abascunana <91748309+abascunana@users.noreply.github.com>
2022-06-03 19:14:49 +0200

    Update localcritic.js

--- Web LocalCritic/localcritic.js
+++ Web LocalCritic/localcritic.js
@@ -81,23 +81,7 @@ function cercar(){
     http.open("GET","http://localhost:8080/LocalCritic/peli2?titulo="+document.getElementById("llistat").value,true);
     http.send();
 }
-function lista(){
-    var http;
-    var rawlista;
-    let lista;
-    http = new XMLHttpRequest;
 
-    http.onreadystatechange = function (){
-        if (http.readyState==4 && http.status==200){
-            document.getElementById("usuaris").innerHTML=http.responseText;
-        }
-    }
-    var coma = ",";
-   http.open("GET","http://localhost:8080/LocalCritic/lista");
-   rawlista = http;
-  dividirCadena(rawlista,coma);
-    http.send();
-}
 function dividirCadena(cadenaADividir,separador) {
     let arrayDeCadenas = cadenaADividir.split(separador);
     for (var i=0; i < arrayDeCadenas.length; i++) {
